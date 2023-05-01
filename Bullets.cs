bb1ab34bcf0df928b1e99d8a106a779133424170
Hongseojun <127863598+Hongseojun@users.noreply.github.com>
2023-05-01 15:49:34 +0900

    Add files via upload
    -적 오브젝트 추가&적 피격 추가
    -적 애니메이션 추가
    -게임매니저(적 생성) 추가

--- Bullets.cs
+++ Bullets.cs
@@ -1,9 +1,12 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullets : MonoBehaviour
 {
+    public int dmg;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "BorderBullet")
